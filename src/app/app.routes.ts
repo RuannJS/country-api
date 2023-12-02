7f0ff602d04b4c8dab93b5019b169da7e06a3b3f
Ruann Fagundes <ruanndev@gmail.com>
2023-12-01 22:14:57 -0300

    Updated Readme

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { HomeComponent } from '../../home/src/lib/home/home.component';
 import { CountryComponent } from '../../country/src/lib/country/country.component';
 
 export const appRoutes: Route[] = [
-  { path: '', component: HomeComponent, title: 'Welcome' },
+  { path: '', component: HomeComponent, title: 'Countries Explorer' },
   { path: ':country', component: CountryComponent, title: 'Country Details' },
 ];
